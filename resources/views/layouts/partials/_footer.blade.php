6f66a1cb115b899af5fdb25dabdafdf0fc70fce4
ahmedKhaireddine <ahmed_khaireddine@yahoo.com>
2019-08-29 10:53:37 +0200

    Setup and add static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="text-center">
+    <p>
+        &copy; {{ date('Y') }} &middot; Laracarte by
+        <a href="https://twitter.com/etsmo"><i class="fab fa-twitter"></i> etsmo</a> - A
+        <a href="https://laramap.com">Laramap</a> clone app.
+    </p>
+    <p>
+        <b>This app has been builr for learning purposes.</b>
+    </p>
+</footer>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.index')
+
+@section('content')
+    <h1 class="text-center">Map here!!!!</h1>
+@stop
